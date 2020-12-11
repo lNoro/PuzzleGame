65031d72cc6e7c3d34178b1786a59cbc89ad1c4d
lNoro <okay.uenal@tum.de>
2020-12-12 00:28:34 +0100

    Camera Control / Collectable Key / DoorTrigger

---
+++ PuzzleGame/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private bool m_MoveCam = false;
+    private Vector3 m_AimPosition;
+    private void Update()
+    {
+        if (transform.position == m_AimPosition)
+            m_MoveCam = false;
+        
+        if (m_MoveCam)
+        {
+            transform.position = Vector3.Lerp(transform.position, m_AimPosition, Time.deltaTime * 4f);
+        }
+    }
+
+    public void GoToLightRoom()
+    {
+        m_AimPosition = new Vector3(transform.position.x, transform.position.y, 14);
+        m_MoveCam = true;
+    }
+}
---
+++ PuzzleGame/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public GameObject Camera;
+    private void OnTriggerEnter(Collider other)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Camera.GetComponent<CameraControl>().GoToLightRoom();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        throw new NotImplementedException();
+    }
+}
