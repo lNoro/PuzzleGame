09fdf711389f8a104d05b0a900cfc6841b805ade
lNoro <okay.uenal@tum.de>
2020-12-11 22:02:10 +0100

    adding scripts tests
    test

---
+++ PuzzleGame/Assets/Scripts/InvisibleWall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWall : MonoBehaviour
+{
+    private Material m_InvisibleMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_InvisibleMaterial = GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        m_InvisibleMaterial;
+    }
+}
---
+++ PuzzleGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public GameObject PlayerModel;
+    private float m_TowardsY = 0f;
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMovement();
+    }
+
+    private void CheckMovement()
+    {
+        float horMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+        //animator.SetFloat("Walking", Mathf.Abs(horMovement));
+
+        transform.Translate(Vector3.forward * (horMovement * Speed * Time.deltaTime), Space.World);
+        transform.Translate(Vector3.left * (verMovement * Speed * Time.deltaTime), Space.World);
+        
+        if (horMovement > 0f)
+        {
+            m_TowardsY = 0f;
+        }
+        else if (horMovement < 0f)
+        {
+            m_TowardsY = -180f;
+        }
+
+        if (verMovement > 0f)
+        {
+            m_TowardsY = -90f;
+        }
+        else if (verMovement < 0f)
+        {
+            m_TowardsY = 90f;
+        }
+
+        PlayerModel.transform.rotation = Quaternion.Lerp(PlayerModel.transform.rotation, Quaternion.Euler(0f, m_TowardsY, 0f), Time.deltaTime * 10f);
+    }
+}
