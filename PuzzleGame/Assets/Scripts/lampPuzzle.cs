f7392dce0990547ace3ac2531bf3a9a5c90ec5cb
Maximilian.Kamps <max.kamps@t-online.de>
2020-12-12 09:55:15 +0100

    Added Door and LampPuzzle.cs

---
+++ PuzzleGame/Assets/Scripts/lampPuzzle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lampPuzzle : MonoBehaviour
+{
+    public GameObject player;
+    private float viewDist = 3;
+
+    [SerializeField]
+    private KeyCode toggleLampVisibilty;
+
+    [SerializeField]
+    private GameObject pointLight;
+
+    [SerializeField]
+    bool isPuzzleLamp = false;
+
+    [SerializeField]
+    static int puzzleLampsTurnedOn = 0;
+    [SerializeField]
+    static int normalLampsTurnedOn = 0;
+    private bool lampOn = true;
+
+    public static bool puzzleSolved = false;
+
+    void Update() {
+        //Falls alle normalen Lampen aus sind und alle Puzzle Lampen an, lößt man das Puzzle (Dann gehen alle Lampen im Raum an)
+        if(normalLampsTurnedOn == 0 && puzzleLampsTurnedOn == 2){
+            pointLight.SetActive(true);
+            Debug.Log("Solved");
+            puzzleSolved = true;
+        }
+    }
+
+    private void OnMouseOver() {
+        //Schaut ob Distanz zwischen Lampe und Spiele unter einem bestimmten Wert liegt, dann kann man mit der lampe interagieren
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if(dist < viewDist && puzzleSolved == false) {
+            
+            //Lampe wird gehighlighted
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
+
+            if(Input.GetKeyDown(toggleLampVisibilty)) {
+                //Lampe wird aktiviert und abhängig von Lampentyp wird Lampenanzahl inkrementiert 
+                if(lampOn == true){
+                    pointLight.SetActive(true);
+                    lampOn = false;
+                    if(isPuzzleLamp) puzzleLampsTurnedOn++;
+                    else normalLampsTurnedOn++;
+                //Lampe wird deaktiviert und abhängig von Lampentyp wird Lampenanzahl dekrementiert 
+                } else {
+                    pointLight.SetActive(false);
+                    lampOn = true;
+                    if(isPuzzleLamp) puzzleLampsTurnedOn--;
+                    else normalLampsTurnedOn--;
+                }
+
+            }
+        }
+    }
+
+    private void OnMouseExit() {
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
