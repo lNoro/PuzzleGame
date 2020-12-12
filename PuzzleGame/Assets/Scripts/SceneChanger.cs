e171e1ac3ad084c368701256ca3362dfe69854cf
lNoro <okay.uenal@tum.de>
2020-12-13 00:08:29 +0100

    Start Scene

--- PuzzleGame/Assets/Scripts/SceneChanger.cs
+++ PuzzleGame/Assets/Scripts/SceneChanger.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadLevel()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
